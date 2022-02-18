6bd23eb4a583062800d0b07300143d65e1e1fb4a
Maxim Zmeyok <maxim_zmeyok@mail.ru>
2022-02-18 23:03:14 +0300

    feat: solve morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let phrase = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        phrase.push(expr.slice(i, i + 10));
+    }
+    phrase.forEach((item, index) => {
+        let start = item.indexOf('1');
+        let realString = start == -1 ? ' ' : item.slice(start);
+        if (realString == ' ') {
+            phrase.splice(index, 1, realString);
+            return;
+        }
+        let word = [];
+        for (let i = 0; i < realString.length; i += 2) {
+            word.push(realString.slice(i, i + 2));
+        }
+        let wordMorse = word.map(item => item == 10 ? '.' : '-').join('');
+        phrase.splice(index, 1, wordMorse);
+    });
+    let result = phrase.map(item => {
+        for (let key in MORSE_TABLE) {
+            if (item == ' ') {
+                return item;
+            } else if (item == key) {
+                return MORSE_TABLE[key];
+            }
+        }
+    }).join('');
+    return result;
 }
 
 module.exports = {
